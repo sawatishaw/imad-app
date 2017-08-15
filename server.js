b2014625406b473ec38fe8b523ea08759df41790
sawatishaw <30799959+sawatishaw@users.noreply.github.com>
2017-08-15 01:59:18 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one',function(req,res){
+   res.send('Article one is requsted and servered first.');
+});
+
+
+app.get('/article-two',function(req,res){
+   res.send('Article two is requsted and servered second.');
+});
+
+app.get('/article-three',function(req,res){
+   res.send('Article three is requsted and servered third.');
+});
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
